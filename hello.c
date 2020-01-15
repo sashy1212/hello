9b77cc7fe1e4e23be29de90c292a895375a77ad1
AndrewAnderson102 <54691886+AndrewAnderson102@users.noreply.github.com>
2020-01-15 17:54:38 -0600

    Update hello.c
    Added my name and major

--- hello.c
+++ hello.c
@@ -2,5 +2,7 @@
 int main(void) {
 	printf("Mathew Pagan\n");
 	printf("Electrical Engineering\n");
+	printf("My name is Andrew Anderson\n");
+	printf("My major is Computer Science\n");
 	return 0;
 }
